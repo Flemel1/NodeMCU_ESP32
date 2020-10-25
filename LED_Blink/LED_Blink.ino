7830aa9b9a7a6f399a1d44322bba30cd9cfde1f6
LeluRezt <lelurezt@gmail.com>
2020-10-25 15:46:21 +0700

    changed file LED_Blink.uno

--- LED_Blink/LED_Blink.ino
+++ LED_Blink/LED_Blink.ino
@@ -1,6 +1,11 @@
 const int PIN_OUTPUT_MERAH = 16;
 const int PIN_OUTPUT_KUNING = 17;
 const int PIN_OUTPUT_HIJAU = 4;
+const long INTERVAL = 3000;
+int ledState_Merah = LOW;
+int ledState_Kuning = LOW;
+int ledState_Hijau = LOW;
+unsigned long previousMillis = 0;
 
 void setup() {
   pinMode(PIN_OUTPUT_MERAH,OUTPUT);
@@ -10,16 +15,38 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(PIN_OUTPUT_MERAH,HIGH);
-  digitalWrite(PIN_OUTPUT_KUNING,LOW);
-  digitalWrite(PIN_OUTPUT_HIJAU,LOW);
-  delay(7000);
-  digitalWrite(PIN_OUTPUT_MERAH,LOW);
-  digitalWrite(PIN_OUTPUT_KUNING,HIGH);
-  digitalWrite(PIN_OUTPUT_HIJAU,LOW);
-  delay(3000);
-  digitalWrite(PIN_OUTPUT_MERAH,LOW);
-  digitalWrite(PIN_OUTPUT_KUNING,LOW);
-  digitalWrite(PIN_OUTPUT_HIJAU,HIGH);
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis >= INTERVAL)
+    previousMillis = currentMillis;
+  {
+    if(ledState_Merah == LOW && ledState_Kuning == LOW && ledState_Hijau == LOW)
+    {
+      ledState_Merah = HIGH;
+      digitalWrite(PIN_OUTPUT_MERAH,ledState_Merah);
+      digitalWrite(PIN_OUTPUT_KUNING,ledState_Kuning);
+      digitalWrite(PIN_OUTPUT_HIJAU,ledState_Hijau);
+    }
+    else if(ledState_Merah == HIGH && ledState_Kuning == LOW && ledState_Hijau == LOW)
+    {
+      ledState_Merah = LOW;
+      ledState_Kuning = HIGH;
+      digitalWrite(PIN_OUTPUT_MERAH,ledState_Merah);
+      digitalWrite(PIN_OUTPUT_KUNING,ledState_Kuning);
+      digitalWrite(PIN_OUTPUT_HIJAU,ledState_Hijau);
+    }
+    else if(ledState_Merah == LOW && ledState_Kuning == HIGH && ledState_Hijau == LOW)
+    {
+      ledState_Kuning = LOW;
+      ledState_Hijau = HIGH;
+      digitalWrite(PIN_OUTPUT_MERAH,ledState_Merah);
+      digitalWrite(PIN_OUTPUT_KUNING,ledState_Kuning);
+      digitalWrite(PIN_OUTPUT_HIJAU,ledState_Hijau);
+    }
+    else
+    {
+      ledState_Hijau = LOW;
+    }
+  }
   delay(5000);
 }
