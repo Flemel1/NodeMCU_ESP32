7075569b8487437b549a160977ca1fc8409145cf
LeluRezt <lelurezt@gmail.com>
2020-10-29 22:20:30 +0700

    changed PIN and added sketch

--- Turn_on_LED_using_LDR/Turn_on_LED_using_LDR.ino
+++ Turn_on_LED_using_LDR/Turn_on_LED_using_LDR.ino
@@ -1,5 +1,5 @@
-const int LED = 17;
-const int LDR = 32;
+const int LED = 4;
+const int LDR = 26;
 
 void setup() {
   Serial.begin(9600);
@@ -10,7 +10,7 @@ void setup() {
 void loop() {
   int ldrStatus = analogRead(LDR);
   
-  if(ldrStatus <= 350) {
+  if(ldrStatus <= 200) {
     digitalWrite(LED,HIGH);
     Serial.print("LED turn on: ");
     Serial.println(ldrStatus);
